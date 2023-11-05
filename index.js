8ad11a63927fa3fbff121773cbc83d4eed93d36d
quantranbber <quan.tranbb@gmail.com>
2023-11-06 00:58:10 +0700

    fix

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const { S3, AWS } = require('aws-sdk');
 const app = express();
 const port = 3000;
-AWS.config.credentials = new AWS.ECSCredentials({ });
 const s3 = new S3({
-    region: 'ap-northeast-1'
+    region: 'ap-southeast-1'
 });
 
 app.get('/', async (req, res) => {
